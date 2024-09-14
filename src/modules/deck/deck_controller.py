69d16d7f1e11615dfe663fd8d982adc4f30cca06
decker <deckerurbano@gmail.com>
2024-09-14 17:47:37 -0300

    Refactor deck controller to simplify data handling
    Moved the loading and saving logic to the deck module to centralize data management. The deck controller now directly interacts with the simplified `create_deck` and `list_decks` functions, reducing redundancy and improving code readability.
    
    Signed-off-by: decker <deckerurbano@gmail.com>

--- src/modules/deck/deck_controller.py
+++ src/modules/deck/deck_controller.py
@@ -1,16 +1,7 @@
-from src.utils.file_manager import save_data, load_data
-from src.modules.deck.deck import create_deck
-
-decks = load_data()
-
+from src.modules.deck.deck import create_deck, list_decks
 
 def create_deck_controller(deck_name):
-    if create_deck(decks, deck_name):
-        save_data(decks)
-        return True
-    else:
-        return False
-
+    return create_deck(deck_name)
 
 def list_decks_controller():
-    return list(decks.keys())
+    return list_decks()
